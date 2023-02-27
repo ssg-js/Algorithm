28da8e5abc07e5614264242c6d3532187c6c9e12
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-02-27 22:26:47 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,23 @@
+def dfs(v):
+    global cnt
+    passed[v] = True
+
+    for next_v in graph[v]:
+        if not passed[next_v]:
+            cnt += 1
+            dfs(next_v)
+
+
+computers = int(input())
+lines = int(input())
+graph = [[] for _ in range(computers+1)]
+passed = [False] * (computers + 1)
+cnt = 0
+
+for i in range(lines):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs(1)
+print(cnt)

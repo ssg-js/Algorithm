d0bd96ae3d6940f48de17e9566c80a1838929dc5
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-02-27 22:25:17 +0900

    [Silver IV] Title: 제로, Time: 3812 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,11 @@
+stack = []
+for k in range(1, int(input()) + 1):
+    num = int(input())
+    # 0을 만나면 pop
+    if num == 0:
+        stack.pop()
+    # 0이 아니면 추가
+    else:
+        stack.append(num)
+
+print(sum(stack))

82ef03a30ac11beaa7a3e3a2cf0960a580306b24
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-02-27 22:26:29 +0900

    [Bronze II] Title: 유학 금지, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2789. 유학 금지/유학 금지.py
@@ -0,0 +1,10 @@
+mail_context = input()
+
+banned = 'CAMBRIDGE'
+
+for m in mail_context:
+    for b in banned:
+        if m == b:
+            mail_context = mail_context.replace(m, '')
+
+print(mail_context)

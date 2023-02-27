cc6a41ebd4067fc393099f3220500d0dc8e9e9f3
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-02-27 22:26:22 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,12 @@
+croatia_alpha = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+word = input()
+cnt = 0
+for alpha in croatia_alpha:
+    cnt += word.count(alpha)
+    word = word.replace(alpha, '0')
+
+word = word.replace('0', '')
+cnt += len(word)
+
+print(cnt)

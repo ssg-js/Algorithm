d5cb800f41defc754dabd91197fd94d89a01a5d4
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-03-02 22:29:28 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 132 ms, Memory: 115768 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,10 +1,10 @@
-# Fibonacci
 def fibonacci(n):
-    if n <= 1:
+    if n < 2:
         return n
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n-2) + fibonacci(n-1)
 
-n = int(input())
 
-print(fibonacci(n))
+N = int(input())
+ans = fibonacci(N)
 
+print(ans)

e0c7699e2081182998cb5966cd0fe6287c3258fd
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-08-04 22:50:30 +0900

    [Silver I] Title: 자바의 형변환, Time: 6824 ms, Memory: 52768 KB -BaekjoonHub

---
+++ 백준/Silver/25601. 자바의 형변환/자바의 형변환.py
@@ -0,0 +1,36 @@
+# [BOJ] 25601. 자바의 형변환
+# 소요 시간 : 20분
+from collections import deque
+
+n = int(input())
+rel = dict()
+for i in range(n - 1):
+    child, parent = input().split()
+    if parent not in rel:
+        rel[parent] = [child]
+    elif child not in rel[parent]:
+        rel[parent].append(child)
+
+one, two = input().split()
+ans = 0
+
+
+def bfs(node, o): # node에서 넓이우선으로 child 탐색
+    queue = deque()
+    queue.extend(rel[node])
+    while queue:
+        now = queue.popleft()
+        if now == o:
+            return True
+        if now in rel:
+            queue.extend(rel[now])
+    return False
+
+
+if one in rel:
+    if bfs(one, two):
+        ans = 1
+if two in rel:
+    if bfs(two, one):
+        ans = 1
+print(ans)

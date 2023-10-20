9332d8154eb92c96c4ddb250eeb4f131c430f413
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-10-20 23:53:43 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,8 +1,11 @@
 import sys
 N, K = map(int, sys.stdin.readline().split())
-info = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-info.sort(key=lambda x: x[1:], reverse=True)
+info = dict()
+for _ in range(N):
+    num, *arr = (map(int, sys.stdin.readline().split()))
+    info[num] = arr
+n_info = sorted(info.items(), key=lambda x: x[1], reverse=True)
 for i in range(0, N):
-    if info[K-1][1:] == info[i][1:]:
+    if info[K] == n_info[i][1]:
         break
 print(i+1)

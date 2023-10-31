c2c128654c322912ad56d3350905a36d2d7413e4
ssg-js <76690497+ssg-js@users.noreply.github.com>
2023-10-31 20:23:27 +0900

    [Gold IV] Title: 여왕벌, Time: 2704 ms, Memory: 112172 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,37 +1,18 @@
 # [BOJ] 10836. 여왕벌
 # 소요 시간 : 60분
 
-size, days = map(int, input().split())
-board = [[1] * size for _ in range(size)]
-growth_info = [list(map(int, input().split())) for _ in range(days)]
-plus_board = [[0] * size for _ in range(size)]
+import sys
 
+size, days = map(int, sys.stdin.readline().split())
+plus = [1] * (2 * size - 1)
 
-def plus():
-    for i in range(days):
-        x, y = size - 1, 0
-        num_info = []
-        num = 0
-        for v in growth_info[i]:
-            num_info.extend([num] * v)
-            num += 1
-        for value in num_info:
-            plus_board[x][y] += value
-            if x == 0:
-                y += 1
-            else:
-                x -= 1
+for d in range(days):
+    zero, one, two = map(int, sys.stdin.readline().split())
+    for o in range(zero, zero + one):
+        plus[o] += 1
+    for t in range(zero + one, 2 * size - 1):
+        plus[t] += 2
 
-
-def grow():
-    for a in range(size):
-        for b in range(size):
-            if a != 0 and b != 0:
-                plus_board[a][b] = max(plus_board[a][b - 1], *plus_board[a - 1][b - 1:b + 1])
-            board[a][b] += plus_board[a][b]
-
-
-plus()
-grow()
-for k in range(size):
-    print(*board[k])
+temp = plus[size:]
+for i in range(size - 1, -1, -1):
+    print(plus[i], *temp)

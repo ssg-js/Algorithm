954e7611cfd4b1f4535a4bc86e0657ed9524a1ea
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-02-04 20:35:19 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 110540 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,32 +1,31 @@
 import sys
-from collections import deque
 read = sys.stdin.readline
 
 n, m = map(int, read().split())
 board = [list(read().rstrip()) for _ in range(n)]
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
-solved = False
 
-queue = deque([(0, 0)])
-move = 1
-while queue:
-    if solved:
-        break
-    for _ in range(len(queue)):
-        if solved:
-            break
-        i, j = queue.popleft()
-        for d in range(4):
-            ni = i + di[d]
-            nj = j + dj[d]
-            if i == n - 1 and j == m - 1:  # arrive
-                print(move)
-                solved = True
-                break
-            if 0 <= ni < n and 0 <= nj < m:
-                if board[ni][nj] == '1':
-                    queue.append((ni, nj))
-                    board[ni][nj] = '0'
-    move += 1
+
+def bfs():
+    queue = [(0, 0)]
+    move = 1
+    while queue:
+        temp = []
+        for i, j in queue:
+            for d in range(4):
+                ni = i + di[d]
+                nj = j + dj[d]
+                if i == n - 1 and j == m - 1:  # arrive
+                    return move
+                if 0 <= ni < n and 0 <= nj < m:
+                    if board[ni][nj] == '1':
+                        temp.append((ni, nj))
+                        board[ni][nj] = '0'
+        queue = temp
+        move += 1
+
+
+print(bfs())
+
 

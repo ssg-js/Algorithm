e2a4c27ce9798cf8f97e7103b8b5a19379139b2a
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-02-04 20:29:35 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 114000 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,35 +3,30 @@ from collections import deque
 read = sys.stdin.readline
 
 n, m = map(int, read().split())
-board = [read().rstrip() for _ in range(n)]
+board = [list(read().rstrip()) for _ in range(n)]
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
-visited = [[False] * m for _ in range(n)]
-visited[0][0] = True
 solved = False
 
-queue = deque()
-queue.append((0,0))
+queue = deque([(0, 0)])
 move = 1
 while queue:
     if solved:
         break
     for _ in range(len(queue)):
-        i, j = queue.popleft()
-        if i == n - 1 and j == m - 1: # arrive
-            print(move)
-            solved = True
+        if solved:
             break
+        i, j = queue.popleft()
         for d in range(4):
             ni = i + di[d]
             nj = j + dj[d]
+            if i == n - 1 and j == m - 1:  # arrive
+                print(move)
+                solved = True
+                break
             if 0 <= ni < n and 0 <= nj < m:
-                if board[ni][nj] == '1' and not visited[ni][nj]:
-                    visited[ni][nj] = True
+                if board[ni][nj] == '1':
                     queue.append((ni, nj))
+                    board[ni][nj] = '0'
     move += 1
-
-
-
-
 

8764f2f2cb81c2b76696e3dfe3b7f663ae1bae03
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-02-14 02:24:07 +0900

    [Gold IV] Title: 최단경로, Time: 456 ms, Memory: 135728 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,42 +1,27 @@
 import sys
-from heapq import heappush, heappop
-
+from heapq import heappop, heappush
 read = sys.stdin.readline
-v_max = 300000 * 10
-
+INF = 300000 * 10
 v, e = map(int, read().split())
 start = int(read())
 board = [[] for _ in range(v+1)]
-
-for _ in range(e):  # 간선 정보 입력
-    a, b, w = map(int, read().split())
-    board[a].append((w, b))
-
-
-def dijkstra(s):
-    visited = [False] * (v+1)
-    d = [v_max] * (v+1)
-    d[s] = 0
-    heap = [(0, s)]
-    while heap:
-        weight, node = heappop(heap)
-        if visited[node]:
-            continue
-        visited[node] = True
-
-        if weight > d[node]:
-            continue
-
-        # 갱신
-        for n_w, n_b in board[node]:
-            if d[n_b] > weight + n_w:
-                d[n_b] = weight + n_w
-                heappush(heap, (weight + n_w, n_b))
-    return d[1:]
-
-
-for ans in dijkstra(start):
-    if ans == v_max:
+for _ in range(e):
+    a, d, w = map(int, read().split())
+    board[a].append((d, w))
+dis = [INF] * (v+1)
+# dijkstra
+dis[start] = 0
+heap = [(0, start)]
+while heap:
+    now_d, now_n = heappop(heap)
+    if now_d > dis[now_n]:
+        continue
+    for next_n, next_d in board[now_n]:
+        if dis[next_n] > now_d + next_d:
+            dis[next_n] = now_d + next_d
+            heappush(heap, (now_d + next_d, next_n))
+for d in dis[1:]:
+    if d == INF:
         print("INF")
     else:
-        print(ans)
+        print(d)

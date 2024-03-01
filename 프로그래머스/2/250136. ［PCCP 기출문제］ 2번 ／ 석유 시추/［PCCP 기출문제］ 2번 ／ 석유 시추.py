4d0f17ea3f5fe35251365c9f7f71631f18cb6281
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-03-01 10:51:47 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 139.14 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -0,0 +1,51 @@
+def solution(land):
+    n = len(land)
+    m = len(land[0])
+    answer = 0
+    board = [[[] for _ in range(m)] for _ in range(n)] # (집합값, 총크기)
+    visited = [[False] * m for _ in range(n)]
+    d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    stack = []
+    store = []
+    # board 만들기
+    ab_num = 1
+    cnt = 0
+    for num in range(n * m):
+        i, j = num // m, num % m
+        if land[i][j] == 0:
+            continue
+        if visited[i][j]:
+            continue
+        stack.append((i, j))
+        store.append((i, j))
+        visited[i][j] = True
+        cnt += 1
+        while stack:
+            x, y = stack.pop()
+            for di, dj in d:
+                ni, nj = x+di, y+dj
+                if 0 <= ni < n and 0 <= nj < m and land[ni][nj] == 1 and not visited[ni][nj]:
+                    cnt += 1
+                    stack.append((ni, nj))
+                    store.append((ni, nj))
+                    visited[ni][nj] = True
+        while store:
+            x, y = store.pop()
+            board[x][y] = (ab_num, cnt)
+        store = []
+        stack = []
+        ab_num += 1
+        cnt = 0
+    for j in range(m):
+        new_visited = set()
+        cnt = 0
+        for i in range(n):
+            if not board[i][j]:
+                continue
+            num, size = board[i][j]
+            if num in new_visited:
+                continue
+            new_visited.add(num)
+            cnt += size
+        answer = max(answer, cnt)
+    return answer

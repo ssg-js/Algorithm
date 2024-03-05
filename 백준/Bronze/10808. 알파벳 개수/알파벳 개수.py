85a3e8e76b828a05d54c79d742ce7580867f78be
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-03-05 16:19:38 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,6 @@
+import sys
+read = sys.stdin.readline
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+s = read().rstrip()
+for a in alpha:
+    print(s.count(a), end=' ')

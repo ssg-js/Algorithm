93f4fc97c261500acce72e94e937b369befe3dd5
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-03-07 20:11:33 +0900

    [Silver II] Title: DFS와 BFS, Time: 164 ms, Memory: 114320 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,42 +1,43 @@
-# N: 정점개수, M: 간선개수, V: 시작정점 번호
-N, M, V = map(int, input().split())
-edges = [[] for _ in range(N + 1)]
-for i in range(M):
-    a, b = map(int, input().split())
-    edges[a].append(b)
-    edges[b].append(a)
-
-
-def dfs(v):
+import sys
+from collections import deque
+read = sys.stdin.readline
+n, e, v = map(int, read().split())
+edge = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+for _ in range(e):
+    a, b = map(int, read().split())
+    edge[a].append(b)
+    edge[b].append(a)
+for i in range(len(edge)):
+    edge[i].sort()
+    
+   
+def dfs(v, visited, arr):
+    if visited[v]:
+        return
     visited[v] = True
-    list_dfs.append(v)
-    for next_v in sorted(edges[v]):
+    print(v, end=' ')
+    for next_v in arr[v]:
         if not visited[next_v]:
-            dfs(next_v)
+            dfs(next_v, visited, arr)
+    return
 
 
-def bfs(v):
+def bfs(v, arr):
+    queue = deque()
+    queue.append(v)
+    visited = [False] * (n+1)
     visited[v] = True
-    queue = [v]
     while queue:
-        now_v = queue.pop(0)
-        list_bfs.append(now_v)
-        for next_v in sorted(edges[now_v]):
+        v = queue.popleft()
+        print(v, end=' ')
+        for next_v in arr[v]:
             if not visited[next_v]:
                 visited[next_v] = True
                 queue.append(next_v)
+    return
 
-
-visited = [False] * (N + 1)
-list_dfs = []
-list_bfs = []
-
-dfs(V)
-visited = [False] * (N + 1)
-
-bfs(V)
-
-print(*list_dfs)
-print(*list_bfs)
-
-
+               
+dfs(v, visited, edge)
+print()
+bfs(v, edge)

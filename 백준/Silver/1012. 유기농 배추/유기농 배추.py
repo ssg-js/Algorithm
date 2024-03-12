c919756952ceae0bc0f72b591466281644c83cbe
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-03-12 21:01:10 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 111456 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,28 @@
-# 유기농 배추
 import sys
-
-sys.setrecursionlimit(10000)
-
-
-def dfs(x, y):
-    visited[x][y] = True
-
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-
-        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and field[nx][ny]:
-            dfs(nx, ny)
-
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for t in range(1, int(input()) + 1):
-    m, n, k = map(int, input().split())
-
-    field = [[0] * m for _ in range(n)]
-    visited = [[False] * m for _ in range(n)]
-    total = 0
-
+read = sys.stdin.readline
+for _ in range(int(read())): # 테스트 케이스
+    m, n, k = map(int, read().split())
+    board = [[0]*m for _ in range(n)] # 밭 상태
+    ans = 0
     for _ in range(k):
-        j, i = map(int, input().split())
-        field[i][j] = 1
-
-    for i in range(n):
-        for j in range(m):
-            if not visited[i][j] and field[i][j] == 1:
-                dfs(i, j)
-                total += 1
-
-    print(total)
+        x, y = map(int, read().split())
+        board[y][x] = 1
+    visited = [[False] * m for _ in range(n)]
+    d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for idx in range(m*n):
+        i, j = idx // m, idx % m
+        if board[i][j] == 0 or visited[i][j]:
+            continue
+        stack = [(i, j)]
+        while stack:
+            x, y = stack.pop()
+            if visited[x][y]:
+                continue
+            visited[x][y] = True
+            for dx, dy in d:
+                nx, ny = x+dx, y+dy
+                if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                    if board[nx][ny] == 1:
+                        stack.append((nx, ny))
+        ans += 1
+    print(ans)

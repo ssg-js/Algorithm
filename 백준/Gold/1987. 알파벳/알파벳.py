cd94223daa7344e7bc99c2bcf8a9bedbafd134b2
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-03-12 20:31:27 +0900

    [Gold IV] Title: 알파벳, Time: 5712 ms, Memory: 168840 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -5,17 +5,21 @@ n, m = map(int, read().split())
 board = [read().rstrip() for _ in range(n)] # 알파벳 입력받기
 ans = 1 # 시작점부터 카운트
 d = [(0, 1), (0, -1), (1, 0), (-1, 0)] # 4방향
+visited = [0] * 26
+visited[ord(board[0][0])-ord('A')] = 1
 
 
-def dfs(x=0, y=0, s=board[0][0]):
+def dfs(x=0, y=0, cnt=1):
     global ans
     if ans == 26: # 최대 26글자임
         return
-    ans = max(ans, len(s))
+    ans = max(ans, cnt)
     for dx, dy in d:
         nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < m and board[nx][ny] not in s:
-            dfs(nx, ny, s + board[nx][ny])
+        if 0 <= nx < n and 0 <= ny < m and not visited[ord(board[nx][ny]) - ord('A')]:
+            visited[ord(board[nx][ny]) - ord('A')] = 1
+            dfs(nx, ny, cnt + 1)
+            visited[ord(board[nx][ny]) - ord('A')] = 0
 
 
 dfs()

f9f1cc98c8ea0e6e62629724aaa69c5f6667dda1
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-03-12 20:25:09 +0900

    [Gold IV] Title: 알파벳, Time: 6184 ms, Memory: 173280 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,28 +1,22 @@
 import sys
 read = sys.stdin.readline
+
 n, m = map(int, read().split())
-board = [read().rstrip() for _ in range(n)]
-ans = 1
-d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-visited = [[False]*m for _ in range(n)]
-visited[0][0] = True
+board = [read().rstrip() for _ in range(n)] # 알파벳 입력받기
+ans = 1 # 시작점부터 카운트
+d = [(0, 1), (0, -1), (1, 0), (-1, 0)] # 4방향
 
 
 def dfs(x=0, y=0, s=board[0][0]):
     global ans
-    if ans == 26:
+    if ans == 26: # 최대 26글자임
         return
     ans = max(ans, len(s))
     for dx, dy in d:
-        nx, ny = x+dx, y+dy
-        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-            if board[nx][ny] in s:
-                continue
-            else:
-                visited[nx][ny] = True
-                dfs(nx, ny, s+board[nx][ny])
-                visited[nx][ny] = False
-                
-                
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < m and board[nx][ny] not in s:
+            dfs(nx, ny, s + board[nx][ny])
+
+
 dfs()
 print(ans)

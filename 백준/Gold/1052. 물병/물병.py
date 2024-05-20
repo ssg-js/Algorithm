83284f3d04ca54bf49d1c0a0ed30457440d8d2fd
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2024-05-20 23:06:53 +0900

    [Gold V] Title: 물병, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -2,22 +2,8 @@ import sys
 read = sys.stdin.readline
 n, k = map(int, read().split())
 ans = 0
-
-
-def check(): # n의 2진수에서 1의 갯수가 k이하인지 체크
-    cnt = 0
-    for p in range(25): # 2^24 = 16,777,216
-        if 2 ** p > n:
-            break
-        if n & (1 << p) == (1 << p): # 1 갯수 카운트
-            cnt += 1
-    if cnt <= k:
-        return False
-    else:
-        return True
-
-
-while check():
-    ans += 1
-    n += 1
+while bin(n).count('1') > k:
+    i = bin(n)[::-1].index('1')
+    ans += 2**i
+    n += 2**i
 print(ans)

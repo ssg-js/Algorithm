0cc6358a8a9ac73a206da4567d2d00a77443313c
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2024-07-13 16:25:34 +0900

    [level 2] Title: 요격 시스템, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -0,0 +1,14 @@
+function solution(targets) {
+    var answer = 0;
+    targets.sort((a, b) => {
+        return a[1] - b[1];
+    })
+    var end = 0;
+    for (var point of targets) {
+        if (point[0] >= end) {
+            answer += 1;
+            end = point[1];
+        }
+    }
+    return answer
+}

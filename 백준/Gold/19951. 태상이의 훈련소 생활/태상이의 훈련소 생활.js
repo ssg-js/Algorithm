fe8685bf366913c7bf8340ca1a5ca988d47fc0ec
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2024-07-23 16:26:33 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 4996 ms, Memory: 42884 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.js
@@ -0,0 +1,20 @@
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./sample.txt").toString().trim().split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+const initBoard = input[1].split(" ").map(Number);
+const prefixSum = [...Array(n)].map(() => 0);
+
+for (let i = 2; i < 2 + m; i++) {
+  let [start, end, value] = input[i].split(" ").map(Number);
+
+  start -= 1;
+  end -= 1;
+  prefixSum[start] += value;
+  if (end + 1 < n) { prefixSum[end + 1] += -value; }
+}
+
+process.stdout.write(initBoard[0] + prefixSum[0] + " ");
+for (let i = 1; i < n; i++) {
+  prefixSum[i] = prefixSum[i - 1] + prefixSum[i];
+  process.stdout.write(initBoard[i] + prefixSum[i] + " ");
+}

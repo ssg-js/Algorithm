500b43d624574753325c235c8643e7e9052fcb3a
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2024-10-03 13:33:32 +0900

    [Gold II] Title: 회의준비, Time: 168 ms, Memory: 11920 KB -BaekjoonHub

--- 백준/Gold/2610. 회의준비/회의준비.js
+++ 백준/Gold/2610. 회의준비/회의준비.js
@@ -3,88 +3,54 @@ const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/st
 
 let n = Number(input[0]);
 let m = Number(input[1]);
-const edges = [...Array(n + 1)].map(() => []);
-const groupNum = [...Array(n + 1)].map((_, i) => i);
+const INF = 100000
+let table = [...Array(n + 1)].map(() => [...Array(n + 1)].map(() => INF))
+for (let i = 0; i < table.length; i++) {
+  table[i][i] = 0;
+}
+for (let i = 2; i < 2 + m; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+  table[a][b] = 1;
+  table[b][a] = 1;
+}
+
+for (let k = 1; k < n + 1; k++) {
+  for (let i = 1; i < n + 1; i++) {
+    for (let j = 1; j < n + 1; j++) {
+      if (i === j || i === k || j === k) { continue; }
+      table[i][j] = Math.min(table[i][j], table[i][k] + table[k][j]);
+    }
+  }
+}
+
 let cnt = 0;
-const answer = [];
+let repNum = [];
 const visited = [...Array(n + 1)].map(() => false);
-
-for (let i = 0; i < m; i++) {
-  let [a, b] = input[i + 2].split(" ").map(Number);
-  edges[a].push(b);
-  edges[b].push(a);
-  union(a, b);
-}
 for (let i = 1; i < n + 1; i++) {
-  find(i);
-}
-
-
-for (let i = 1; i < groupNum.length; i++) {
   if (visited[i] === true) { continue; }
   cnt++;
-  const iter = [];
-  let retNum = 0;
-  let minDeliveryTime = 10000;
-  let num = groupNum[i];
-  // 같은 그룹애들 구하기
-  for (let j = 1; j <= groupNum.length; j++) {
-    if (groupNum[j] === num) {
-      iter.push(j);
-      visited[j] = true;
+  const sameGroup = [];
+  for (let k = i; k < n + 1; k++) {
+    if (table[i][k] !== INF) {
+      sameGroup.push(k);
+      visited[k] = true;
     }
   }
 
-  // 대표자 선정
-  for (let chosen of iter) {
-    let tempTime = bfs(chosen);
-    if (tempTime < minDeliveryTime) {
-      minDeliveryTime = tempTime;
-      retNum = chosen;
+  let minTime = INF;
+  let minK = INF;
+  for (let k of sameGroup) {
+    let maxTime = 0;
+    for (let nk of sameGroup) {
+      maxTime = Math.max(maxTime, table[k][nk]);
+    }
+    if (maxTime < minTime) {
+      minTime = maxTime;
+      minK = k;
     }
   }
-
-  answer.push(retNum);
-}
-answer.sort((a, b) => a - b);
-console.log([cnt, ...answer].join('\n'));
-
-function find(p) {
-  if (groupNum[p] === p) { return p; }
-  groupNum[p] = find(groupNum[p]);
-  return groupNum[p];
+  repNum.push(minK);
 }
 
-function union(a, b) {
-  na = find(a);
-  nb = find(b);
-  if (na < nb) {
-    groupNum[nb] = na;
-  } else {
-    groupNum[na] = nb;
-  }
-}
-
-function bfs(k) {
-  let queue = [k];
-  let idx = 0;
-  let cnt = 0;
-  let visited = [...Array(n + 1)].map(() => false);
-  visited[k] = true;
-  while (queue.length > idx) {
-    let limit = queue.length - idx;
-    for (let i = 0; i < limit; i++) {
-      let cur = queue[idx];
-      idx++;
-      for (let nk of edges[cur]) {
-        if (visited[nk] === false) {
-          queue.push(nk);
-          visited[nk] = true;
-        }
-      }
-    }
-    cnt++;
-  }
-
-  return cnt;
-}
+repNum.sort((a, b) => a - b);
+console.log([cnt, ...repNum].join('\n'))

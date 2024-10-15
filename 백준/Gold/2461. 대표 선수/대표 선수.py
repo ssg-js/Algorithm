1aeaffec4ea70eb46643cec1d7a9ff724c2df386
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2024-10-15 15:43:06 +0900

    [Gold II] Title: 대표 선수, Time: 1308 ms, Memory: 123700 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.py
+++ 백준/Gold/2461. 대표 선수/대표 선수.py
@@ -12,19 +12,19 @@ ans = 1000000000
 
 idx = [0] * n
 min_pq = []
-max_pq = []
+max_value = -1
 for c in range(n):
     heapq.heappush(min_pq, (students[c][idx[c]], c))
-    heapq.heappush(max_pq, (-students[c][idx[c]], c))
+    max_value = max(max_value, students[c][idx[c]])
 
 
 while max(idx) < m:
-    ans = min(ans, -max_pq[0][0] - min_pq[0][0])
+    ans = min(ans, max_value - min_pq[0][0])
     cur_v, cur_c = heapq.heappop(min_pq)
     idx[cur_c] += 1
     if idx[cur_c] < m:
         heapq.heappush(min_pq, (students[cur_c][idx[cur_c]], cur_c))
-        heapq.heappush(max_pq, (-students[cur_c][idx[cur_c]], cur_c))
+        max_value = max(max_value, students[cur_c][idx[cur_c]])
     else:
         break
 

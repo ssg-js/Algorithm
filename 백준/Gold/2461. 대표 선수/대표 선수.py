6f51b7e1d2792036bfe5489e0c4ef4e25bd7af0e
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2024-10-15 15:38:57 +0900

    [Gold II] Title: 대표 선수, Time: 1500 ms, Memory: 219596 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.py
+++ 백준/Gold/2461. 대표 선수/대표 선수.py
@@ -1,6 +1,7 @@
 import sys
 read = sys.stdin.readline
 
+import heapq
 n, m = map(int, read().split())
 students = []
 for i in range(n):
@@ -10,17 +11,21 @@ for i in range(n):
 ans = 1000000000
 
 idx = [0] * n
+min_pq = []
+max_pq = []
+for c in range(n):
+    heapq.heappush(min_pq, (students[c][idx[c]], c))
+    heapq.heappush(max_pq, (-students[c][idx[c]], c))
+
 
 while max(idx) < m:
-    
-    to_change_class = 0
-    max_value = students[0][idx[0]]
-    for c in range(1, n):
-        if students[c][idx[c]] > max_value:
-            max_value = students[c][idx[c]]
-        if students[c][idx[c]] < students[to_change_class][idx[to_change_class]]:
-            to_change_class = c
-    ans = min(ans, max_value - students[to_change_class][idx[to_change_class]])
-    idx[to_change_class] += 1
+    ans = min(ans, -max_pq[0][0] - min_pq[0][0])
+    cur_v, cur_c = heapq.heappop(min_pq)
+    idx[cur_c] += 1
+    if idx[cur_c] < m:
+        heapq.heappush(min_pq, (students[cur_c][idx[cur_c]], cur_c))
+        heapq.heappush(max_pq, (-students[cur_c][idx[cur_c]], cur_c))
+    else:
+        break
 
 print(ans)

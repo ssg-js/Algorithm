e4423a0f800ee437121f342cc5d4442922fe3c6b
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2024-10-27 12:51:16 +0900

    [Silver I] Title: 태권왕, Time: 116 ms, Memory: 11404 KB -BaekjoonHub

---
+++ 백준/Silver/14562. 태권왕/태권왕.js
@@ -0,0 +1,37 @@
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./sample.txt").toString().trim().split("\n");
+
+const t = Number(input[0]);
+for (let i = 1; i < 1 + t; i++) {
+  let [s, t] = input[i].split(" ").map(Number);
+  const board = [...Array(124)].map(() => []); // 도착지점을 저장해 해당 도착지점이 맞는 case마다 visited 처리
+  board[s].push(t);
+  const queue = [[s, t, 0]];
+  let queueIdx = 0;
+  let cnt, cur, target;
+
+  while (queueIdx < queue.length) {
+    [cur, target, cnt] = queue[queueIdx];
+    queueIdx += 1;
+
+    // A
+    if (cur * 2 === target + 3) {
+      console.log(cnt + 1);
+      break;
+    }
+    if (cur * 2 < target + 3) {
+      if (cur * 2 < board.length && !(target + 3 in board[cur * 2])) {
+        board[cur * 2].push(target + 3);
+        queue.push([cur * 2, target + 3, cnt + 1]);
+      }
+    }
+    // B
+    if (cur + 1 === target) {
+      console.log(cnt + 1);
+      break;
+    }
+    if (cur + 1 < board.length && !(target in board[cur + 1])) {
+      board[cur + 1].push(target);
+      queue.push([cur + 1, target, cnt + 1]);
+    }
+  }
+}

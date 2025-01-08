47a41cc4ece64ff177a26282faf657020bfe22ce
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2025-01-08 21:22:53 +0900

    [level 4] Title: 지형 이동, Time: 224.17 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.js
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.js
@@ -1,71 +1,61 @@
 function solution(land, height) {
-    var answer = 0;
+    let answer = 0;
     const n = land.length;
-    const infoBoard = [...Array(n)].map(()=>[...Array(n)].map(()=>0)); // 0이면 방문안한곳
+    const visited = [...Array(n)].map(()=>[...Array(n)].map(()=>false));
     const delta = [[-1, 0], [1, 0], [0, 1], [0, -1]];
-    let setNum = 0;
-    // height 이하인 점끼리 집합 나누기
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (infoBoard[i][j] === 0) { // 처리안한곳 찾기
-                setNum += 1;
-                infoBoard[i][j] = setNum;
-                const arr = [[i, j]];
-                while (arr.length > 0) {
-                    let [x, y] = arr.pop();
-                    for (let [dx, dy] of delta) {
-                        let [nx, ny] = [x+dx, y+dy];
-                        if (0 <= nx && nx < n && 0 <= ny && ny < n && infoBoard[nx][ny] === 0 && Math.abs(land[nx][ny]-land[x][y]) <= height) {
-                            arr.push([nx, ny]);
-                            infoBoard[nx][ny] = setNum;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    // 다른 집합인 곳 인접한 점끼리 [차이, 집합1, 집합2] 로 저장(heap 쓰면 빠름)
-    const store = [];
-    for (let x = 0; x < n; x++) {
-        for (let y = 0; y < n; y++) {
-            let [nx, ny] = [x, y+1]; // 오른쪽
-            if (0 <= nx && nx < n && 0 <= ny && ny < n && infoBoard[nx][ny] !== infoBoard[x][y]) { // 집합 다르면
-                store.push([Math.abs(land[nx][ny] - land[x][y]), [x, y], [nx, ny]]);
-            }
-            [nx, ny] = [x+1, y]; // 아래
-            if (0 <= nx && nx < n && 0 <= ny && ny < n && infoBoard[nx][ny] !== infoBoard[x][y]) { // 집합 다르면
-                store.push([Math.abs(land[nx][ny] - land[x][y]), [x, y], [nx, ny]]);
-            }
-        }
-    }
-    store.sort((a, b) => a[0]-b[0]);
-    // 최소 비용으로 모든 집합 연결
-    const parents = [...Array(setNum+1)].map((_, i)=>i); // 각 집합의 합쳐진 집합 번호 저장(안 합쳐지면 자기 번호)
-    let cnt = 0;
-    for (let i = 0; i < store.length; i++) {
-        let [v, a, b] = store[i];
-        let [aNum, bNum] = [infoBoard[a[0]][a[1]], infoBoard[b[0]][b[1]]];
-        let [aSetNum, bSetNum] = [find(parents, aNum), find(parents, bNum)];
-        
-        if (aSetNum === bSetNum) { continue; } // 같은 집합이면 넘어가기
-        // 다른 집합인 경우
+    const heap = [[0, 0, 0]];
+    while (heap.length > 0) {
+        let [v, x, y] = minHeapPop(heap);
+        if (visited[x][y]) { continue; }
+        visited[x][y] = true;
         answer += v;
-        cnt += 1;
-        // 둘 비교해서 숫자가 큰 집합애들 작은 집합으로 변환
-        union(parents, aSetNum, bSetNum);
-        // setNum-1 만큼 돌았으면 끝
-        if (cnt === setNum-1) { break; }
+        for (let [dx, dy] of delta) {
+            let [nx, ny] = [x+dx, y+dy];
+            if (0 <= nx && nx < n && 0 <= ny && ny < n && !visited[nx][ny]) {
+                let sub = Math.abs(land[x][y]-land[nx][ny]);
+                minHeapPush(heap, [(sub <= height ? 0 : sub), nx, ny]);
+            }
+        }
     }
+    
     return answer;
 }
 
-function union(arr, x, y) {
-    if (x < y) { arr[y] = x; }
-    else if (x > y) { arr[x] = y;}
-    return;
+function minHeapPush(arr, node) {
+    arr.push(node);
+    let idx = arr.length - 1; // 힙에서 가장 마지막 위치
+    while (idx > 0 && arr[Math.floor((idx-1)/2)][0] > arr[idx][0]) { // 현재 num이 num의 부모노드보다 작으면 교체
+        arr[idx] = arr[Math.floor((idx-1)/2)];
+        arr[Math.floor((idx-1)/2)] = node;
+        idx = Math.floor((idx-1)/2);
+    }
 }
 
-function find(arr, x) {
-    if (arr[x] === x) { return x; }
-    return find(arr, arr[x]);
+function minHeapPop(arr) {
+    if (arr.length === 0) { 
+        console.log('heap is empty');
+        return NaN;
+    }
+    // 힙에서 제일 처음 위치랑 마지막 위치 교환
+    if (arr.length === 1) { return arr.pop(); }
+    let temp = arr[arr.length-1];
+    arr[arr.length-1] = arr[0];
+    arr[0] = temp;
+    const ret = arr.pop();
+    let idx = 0;
+    while (idx < arr.length) {
+        const [llIdx, rlIdx] = [2*idx+1, 2*idx+2];
+        if (llIdx >= arr.length) { break; }
+        let minChild = llIdx;
+        if (rlIdx < arr.length && arr[rlIdx][0] < arr[llIdx][0]) {
+            minChild = rlIdx;
+        }
+        if (arr[idx][0] > arr[minChild][0]) {
+            let temp = arr[idx];
+            arr[idx] = arr[minChild];
+            arr[minChild] = temp
+            idx = minChild;
+        } else { break; }
+    }
+    return ret;
 }

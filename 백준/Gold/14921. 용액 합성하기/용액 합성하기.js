a4116b7bea9d031dc6cbb1b7ec61b5fa9c264319
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2025-03-09 20:35:20 +0900

    [Gold V] Title: 용액 합성하기, Time: 176 ms, Memory: 19968 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.js
@@ -0,0 +1,19 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "sample.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+arr.sort((a, b) => a - b);
+let answer = Infinity;
+let [left, right] = [0, n - 1];
+while (left < right) {
+  let result = arr[left] + arr[right];
+  if (Math.abs(result) < Math.abs(answer)) { answer = result; }
+  if (result === 0) { break; }
+  else if (result > 0) { right -= 1; }
+  else { left += 1; }
+}
+console.log(answer);

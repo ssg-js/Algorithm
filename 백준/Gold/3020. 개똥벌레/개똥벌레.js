13770c7d3b2348751d1c9b34187131743c1faf6b
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2025-03-20 21:39:00 +0900

    [Gold V] Title: 개똥벌레, Time: 228 ms, Memory: 29176 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "sample.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, h] = input[0].split(" ").map(Number);
+const board = Array(h).fill(0);
+for (let i = 0; i < n; i++) {
+  let size = Number(input[1 + i]);
+  if (i % 2 === 0) { // 석순 : idx 0 부터 size전까지
+    board[0] += 1;
+    board[size] -= 1;
+  } else { // 종유석 : idx h-size부터 n전까지
+    board[h - size] += 1;
+  }
+}
+
+let minObs = board[0];
+let count = 1;
+for (let i = 1; i < h; i++) {
+  board[i] += board[i - 1];
+  if (board[i] === minObs) {
+    count++;
+  } else if (board[i] < minObs) {
+    minObs = board[i];
+    count = 1;
+  }
+}
+
+
+console.log(minObs, count);

9b6bd33dc6f372f6c45c9ccad6c86172f1d06cac
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2025-04-02 14:13:34 +0900

    [Gold V] Title: 합분해, Time: 168 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.js
+++ 백준/Gold/2225. 합분해/합분해.js
@@ -9,16 +9,16 @@ const dp = [...Array(k)].map(() => Array(n + 1).fill(0n));
 
 // first line
 for (let j = 0; j < n + 1; j++) {
-  dp[0][j] = 1n;
+  dp[0][j] = BigInt(j + 1);
 }
 
 // other lines
 for (let i = 1; i < k; i++) {
   for (let j = 0; j < n + 1; j++) {
-    for (let prev = j; prev >= 0; prev--) {
-      dp[i][j] += dp[i - 1][prev];
-    }
+    dp[i][j] += dp[i - 1][j];
+    if (j > 0) dp[i][j] += dp[i][j - 1];
+    ``;
   }
 }
 
-console.log(String(dp[k - 1][n] % 1000000000n));
+console.log(String((dp[k - 1][n] - dp[k - 1][n - 1]) % 1000000000n));

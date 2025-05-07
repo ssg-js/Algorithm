845aa6b413b3128f75d73f944d1f8fafce22157a
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2025-05-07 17:07:07 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 1988 ms, Memory: 371008 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -2,6 +2,9 @@
  * 16933. 벽 부수고 이동하기 3
  * bfs로 방문 처리하며 진행
  * 방문 처리는 낮밤 여부, 좌표, 부술 수 있는 벽의 갯수로 함. 그래서 시간복잡도는 2 * 10^6 * 10 = 2 * 10^7 임
+ * 틀린 부분
+ * -> bfs 4방향 for문에서 각 방향에 대해 cur.rest를 바로 수정해서 다른 방향에 영향을 줌
+ * -> 시작지점과 도착지점이 같은경우
  */
 
 
@@ -11,7 +14,7 @@ import java.util.*;
 public class Main {
     private static int n, m, k;
     private static char[][] board;
-    private static boolean[][][][] visited; // 방문할때 기준
+    private static boolean[][][] visited; // 방문할때 기준
     private static final int[] dx = {-1, 0, 1, 0};
     private static final int[] dy = {0, 1, 0, -1};
 
@@ -32,7 +35,7 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         board = new char[n][m];
-        visited = new boolean[n][m][2][k+1];
+        visited = new boolean[n][m][k+1];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
@@ -54,7 +57,7 @@ public class Main {
         if (n == 1 && m == 1) return 1;
         ArrayDeque<Element> queue = new ArrayDeque<>();
         queue.offer(new Element(0, 0, 1, k, 1));
-        visited[0][0][1][k] = true;
+        visited[0][0][k] = true;
 
         while (!queue.isEmpty()) {
             Element cur = queue.poll();
@@ -66,26 +69,25 @@ public class Main {
                 }
                 if (!check(nx, ny)) continue;
                 int nDay = (cur.day + 1) % 2;
-                int nRest = cur.rest;
                 if (board[nx][ny] == '1') {
                     // wall
-                    if (nRest == 0) continue;
+                    if (cur.rest == 0) continue;
                     if (cur.day == 1) {
                         // 낮이면 벽뚫고 가기
-                        nRest--;
+                        if (visited[nx][ny][cur.rest-1]) continue;
+                        queue.offer(new Element(nx, ny, nDay, cur.rest-1, cur.dist+1));
+                        visited[nx][ny][cur.rest-1] = true;
                     } else {
                         // 밤이면 원래 위치에서 하루 대기
-                        nx = cur.x;
-                        ny = cur.y;
+                        queue.offer(new Element(cur.x, cur.y, nDay, cur.rest, cur.dist+1));
+                        visited[cur.x][cur.y][cur.rest] = true;
                     }
-                    if (visited[nx][ny][nDay][nRest]) continue;
-                    queue.offer(new Element(nx, ny, nDay, nRest, cur.dist+1));
-                    visited[nx][ny][nDay][nRest] = true;
+                    
                 } else {
                     // not wall
-                    if (visited[nx][ny][nDay][nRest]) continue;
-                    queue.offer(new Element(nx, ny, nDay, nRest, cur.dist+1));
-                    visited[nx][ny][nDay][nRest] = true;
+                    if (visited[nx][ny][cur.rest]) continue;
+                    queue.offer(new Element(nx, ny, nDay, cur.rest, cur.dist+1));
+                    visited[nx][ny][cur.rest] = true;
                 }
 
             }

267579af36eabace5c776eef78a4ebca717a190e
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2025-05-23 11:35:30 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 316 ms, Memory: 59676 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
@@ -5,25 +5,34 @@ const input = require("fs")
   .trim()
   .split("\n");
 const n = Number(input[0]);
-const state = input[1].split(" ").map(Number);
+const state = input[1].split(" ").map(BigInt);
 let answer = 0n;
 let sectionSize = 0;
-for (let v of state) {
-  if (v > 0) {
+let firstSectionSize = 0;
+let idx = 0;
+// 1번을 포함한 구역
+for (let i = 0; i < n; i++) {
+  if (state[i] > 0n) break;
+  firstSectionSize++;
+  idx++;
+}
+// 나머지 구역
+for (let i = idx; i < n; i++) {
+  const v = state[i];
+  if (v > 0n) {
     // 쪽방
     let count = Math.floor(sectionSize / 2);
     if (sectionSize % 2 > 0) count++;
-    answer += BigInt(v) + BigInt(count);
+    answer += v + BigInt(count);
     sectionSize = 0;
   } else {
     // 구역
     sectionSize++;
   }
 }
-if (sectionSize > 0) {
-  let count = Math.floor(sectionSize / 2);
-  if (sectionSize % 2 > 0) count++;
-  answer += BigInt(count);
-}
+firstSectionSize += sectionSize;
+let count = Math.floor(firstSectionSize / 2);
+if (firstSectionSize !== n && firstSectionSize % 2 > 0) count++;
+answer += BigInt(count);
 
 console.log(answer.toString());

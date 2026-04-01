2b35ceb46469bc2b1c4ce409c3a7129d26e238b5
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2026-04-01 19:07:06 +0900

    [Gold I] Title: 구간 성분, Time: 800 ms, Memory: 21312 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10840. 구간 성분/구간 성분.js
+++ 백준/Gold/10840. 구간 성분/구간 성분.js
@@ -14,6 +14,14 @@ if (A.length > B.length) {
 const N = A.length;
 const M = B.length;
 
+const hashExpo = Array(26).fill(0);
+let mul = 1n;
+let base = 1501n;
+for (let i = 0; i < 26; i++) {
+  hashExpo[i] = mul;
+  mul *= base;
+}
+
 let answer = 0;
 for (let k = 1; k < N + 1; k++) {
   if (checkPossible(k)) answer = k;
@@ -25,39 +33,43 @@ function checkPossible(k) {
   const hashSet = new Set();
 
   const elementsOfA = Array(26).fill(0);
+  let aHash = 0n;
   // A 신호에서 i부터 k길이의 구간 성분 가져오기
   for (let i = 0; i < N - k + 1; i++) {
     if (i === 0) {
       // 구간 처음 뽑을 때
+      aHash = 0n;
       for (let j = i; j < k; j++) {
+        aHash += hashExpo[aToN(A, j)];
         elementsOfA[aToN(A, j)]++;
       }
     } else {
-      // 첫번째 구간 아니면 성분 배열 업데이트
-      elementsOfA[aToN(A, i - 1)]--;
-      elementsOfA[aToN(A, i + k - 1)]++;
+      // 첫번째 구간 아니면 해쉬 업데이트
+      aHash -= hashExpo[aToN(A, i - 1)];
+      aHash += hashExpo[aToN(A, i + k - 1)];
     }
     // 해싱
-    hashSet.add(getHash(elementsOfA));
+    hashSet.add(aHash);
   }
 
   const elementsOfB = Array(26).fill(0);
+  let bHash = 0n;
   // B에서 k길이의 구간 뽑기
-  for (let j = 0; j < M - k + 1; j++) {
-    if (j === 0) {
+  for (let i = 0; i < M - k + 1; i++) {
+    if (i === 0) {
       // 구간 처음 뽑을 때
-      for (let l = j; l < k; l++) {
-        elementsOfB[aToN(B, l)]++;
+      bHash = 0n;
+      for (let j = i; j < k; j++) {
+        bHash += hashExpo[aToN(B, j)];
       }
     } else {
       // 첫번째 구간 아니면 성분 배열 업데이트
-      elementsOfB[aToN(B, j - 1)]--;
-      elementsOfB[aToN(B, j + k - 1)]++;
+      bHash -= hashExpo[aToN(B, i - 1)];
+      bHash += hashExpo[aToN(B, i + k - 1)];
     }
 
-    let hash = getHash(elementsOfB);
     // A, B 구간 비교
-    if (hashSet.has(hash)) return true;
+    if (hashSet.has(bHash)) return true;
   }
 
   return false;
@@ -65,17 +77,4 @@ function checkPossible(k) {
 
 function aToN(str, i) {
   return str.charCodeAt(i) - "a".charCodeAt();
-}
-
-function getHash(arr) {
-  let ret = 0n;
-  let mul = 1n;
-  let base = 1501n;
-
-  for (let i = 0; i < 26; i++) {
-    ret += mul * BigInt(arr[i]);
-    mul *= base;
-  }
-
-  return ret;
 }

e3be25b15b9a0815c1d025eb81d34fd2c9be1eb8
Jinseong Park <76690497+ssg-js@users.noreply.github.com>
2026-04-01 18:50:35 +0900

    [Gold I] Title: 구간 성분, Time: 1680 ms, Memory: 20896 KB, Score: 30 point -BaekjoonHub

--- 백준/Gold/10840. 구간 성분/구간 성분.js
+++ 백준/Gold/10840. 구간 성분/구간 성분.js
@@ -22,10 +22,11 @@ console.log(answer);
 
 // 파라미터로 들어온 길이의 구간 중에 알파벳 성분이 같은 구간이 있는지 체크
 function checkPossible(k) {
-  const elementsOfA = Array(26).fill(0);
+  const hashSet = new Set();
 
+  const elementsOfA = Array(26).fill(0);
+  // A 신호에서 i부터 k길이의 구간 성분 가져오기
   for (let i = 0; i < N - k + 1; i++) {
-    // A 신호에서 i부터 k길이의 구간 성분 가져오기
     if (i === 0) {
       // 구간 처음 뽑을 때
       for (let j = i; j < k; j++) {
@@ -36,24 +37,27 @@ function checkPossible(k) {
       elementsOfA[aToN(A, i - 1)]--;
       elementsOfA[aToN(A, i + k - 1)]++;
     }
-    const elementsOfB = Array(26).fill(0);
+    // 해싱
+    hashSet.add(getHash(elementsOfA));
+  }
 
-    for (let j = 0; j < M - k + 1; j++) {
-      // B에서 k길이의 구간 뽑기
-      if (j === 0) {
-        // 구간 처음 뽑을 때
-        for (let l = j; l < k; l++) {
-          elementsOfB[aToN(B, l)]++;
-        }
-      } else {
-        // 첫번째 구간 아니면 성분 배열 업데이트
-        elementsOfB[aToN(B, j - 1)]--;
-        elementsOfB[aToN(B, j + k - 1)]++;
+  const elementsOfB = Array(26).fill(0);
+  // B에서 k길이의 구간 뽑기
+  for (let j = 0; j < M - k + 1; j++) {
+    if (j === 0) {
+      // 구간 처음 뽑을 때
+      for (let l = j; l < k; l++) {
+        elementsOfB[aToN(B, l)]++;
       }
-
-      // A, B 구간 비교
-      if (compare(elementsOfA, elementsOfB)) return true;
+    } else {
+      // 첫번째 구간 아니면 성분 배열 업데이트
+      elementsOfB[aToN(B, j - 1)]--;
+      elementsOfB[aToN(B, j + k - 1)]++;
     }
+
+    let hash = getHash(elementsOfB);
+    // A, B 구간 비교
+    if (hashSet.has(hash)) return true;
   }
 
   return false;
@@ -63,10 +67,15 @@ function aToN(str, i) {
   return str.charCodeAt(i) - "a".charCodeAt();
 }
 
-function compare(arr1, arr2) {
-  let size = 26;
-  for (let i = 0; i < size; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+function getHash(arr) {
+  let ret = 0n;
+  let mul = 1n;
+  let base = 1501n;
+
+  for (let i = 0; i < 26; i++) {
+    ret += mul * BigInt(arr[i]);
+    mul *= base;
   }
-  return true;
+
+  return ret;
 }
